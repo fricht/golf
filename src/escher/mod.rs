89578eb4f89ef73851c76b132bde2b06da7c9587
fricht <fricht@users.noreply.github.com>
2024-10-16 16:09:53 +0200

    added Box Ui Component, and same changes

--- src/escher/mod.rs
+++ src/escher/mod.rs
@@ -1,4 +1,6 @@
-use crate::graphics::Buffer;
+pub mod components;
+
+use crate::{graphics::Buffer, math::Vec2i};
 
 /*
 
@@ -18,18 +20,33 @@ Then it sends another signal to it's parent (eg : the same signal if it hasen't 
 
 */
 
+/// not selected color
+pub const IDLE_COLOR: u16 = 57083;
+/// selected color
+pub const SELECT_COLOR: u16 = 48631;
+
 /// ui events
 pub enum UiEvent {
-    Ok,
+    None, // this allows to re-draw everything
+    Ok,   // Ok or Exe
     Up,
     Down,
     Left,
+    Right,
+    Back,
+    Number(u8),
+    Backspace,
 }
 
 pub trait TopLevel {
+    fn update(&mut self);
     fn render(&self, buffer: &mut Buffer);
 }
 
 pub trait Component {
-    fn render(&self, buffer: &mut Buffer);
+    fn update(&mut self, signal: UiEvent) -> UiEvent;
+    fn render(&self, buffer: &mut Buffer, offset: Vec2i);
+    fn requested_size(&self) -> Vec2i;
+    fn set_selected(&mut self, selected: bool);
+    fn get_selected(&self) -> bool;
 }
